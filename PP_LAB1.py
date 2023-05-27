d68bca4698121417fb2f60d1fa73aa1f870d8095
arriagadakevin <arriagadakevin55@gmail.com>
2023-05-26 22:26:18 -0300

    punto 2

--- PP_LAB1.py
+++ PP_LAB1.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def parce_json(nombre_archivo : str) -> list:
     """
@@ -9,30 +10,49 @@ def parce_json(nombre_archivo : str) -> list:
         lista_personaje = dict["jugadores"]
     return lista_personaje
 
-lista_jugadores = parce_json(r"C:\Users\Kevin\Desktop\UTN\1er parcial python\PP_lab1_arriagada_kevin\dt.json")
+lista_jugadores = parce_json(r"dt.json")
+    
+def imprimir_datos(dato)-> None:
+    """
+    funcion para imprimir dato 
+    parametro: dato, 'dato' a imprimir ingresado
+    retorna: dato
+    """
+    print(dato)
 
-def mostrar_jugadores(lista_jugadores:list, punto: str) -> list:
+        
+def mostrar_jugadores(lista_jugadores:list, mostra_indice : bool) -> list:
     """
     mostrar jugadores en formato(Nombre Jugador - Posición)
     parametro: lista_jugadores : list, lista con todos los jugadores a imprimir
     devulve: lista ordenada con el formato deseado
     """
     if len(lista_jugadores) > 0:
-        lista = lista_jugadores[:]
-        if punto == "1":
-            for jugadores in lista:
-                    print("{0} - {1}".format(jugadores["nombre"], jugadores["posicion"]))
-        elif punto == "2":
+        if mostra_indice == False:
+            for jugadores in lista_jugadores:
+                imprimir_datos("{0} - {1}".format(jugadores["nombre"], jugadores["posicion"]))
+        elif mostra_indice == True:
             indice = 0
-            for jugadores in lista:  
-                print("{0} | {1} | {2}".format(indice, jugadores["nombre"], jugadores["posicion"]))
+            for jugadores in lista_jugadores:  
+                imprimir_datos("{0} | {1} | {2}".format(indice, jugadores["nombre"], jugadores["posicion"]))
                 indice += 1
     else:
-        print("[herror]: lista vacia")
+        print("[error]: lista vacia")
 
 
 def mostrar_estadisticas_jugador_por_indice(lista_jugadores : list):
-    pass
+    mostrar_jugadores(lista_jugadores, True)
+    indice = input("selecciones el indice del jugador deseado")
+    while re.search(r"^(0|1|2|3|4|5|6|7|8|9|10|11)$", indice) == None :
+        print("[error] : numero incorrecto")
+        indice = input("selecciones el indice del jugador deseado")
+
+    indice = int(indice)
+    jugador_seleccionado = lista_jugadores[indice]
+    imprimir_datos("el jugador seleccionado es: {0} y sus estadisticas son: {1}".format(
+                                                jugador_seleccionado["nombre"],
+                                                jugador_seleccionado["estadisticas"]))
+
 
 
 
