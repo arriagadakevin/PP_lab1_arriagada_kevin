074fc21ce490c44cba3e6c34cd06ea1578e17416
arriagadakevin <arriagadakevin55@gmail.com>
2023-05-28 22:16:53 -0300

    cambios

--- PP_LAB1.py
+++ PP_LAB1.py
@@ -1,6 +1,14 @@
 import json
 import re
 
+def imprimir_datos(dato)-> None:
+    """
+    funcion para imprimir dato 
+    parametro: dato, 'dato' a imprimir ingresado
+    retorna: dato
+    """
+    print(dato)
+
 def parce_json(nombre_archivo : str) -> list:
     """
     """
@@ -11,14 +19,32 @@ def parce_json(nombre_archivo : str) -> list:
     return lista_personaje
 
 lista_jugadores = parce_json(r"dt.json")
-    
-def imprimir_datos(dato)-> None:
-    """
-    funcion para imprimir dato 
-    parametro: dato, 'dato' a imprimir ingresado
-    retorna: dato
-    """
-    print(dato)
+
+def mostrar_menu()->None:
+    imprimir_datos("""
+    1. Mostrar la lista de todos los jugadores del Dream Team con su posición.
+    2. Permitir al usuario seleccionar un jugador por su índice y mostrar sus estadísticas completas.
+    3. Permitir al usuario guardar las estadísticas de un jugador seleccionado en un archivo CSV.
+    4. Permitir al usuario buscar un jugador por su nombre y mostrar sus logros.
+    5. Calcular y mostrar el promedio de puntos por partido de todo el equipo del Dream Team, ordenado por nombre.
+    6. Permitir al usuario ingresar el nombre de un jugador y mostrar si ese jugador es miembro del Salón de la Fama del Baloncesto.
+    7. Calcular y mostrar el jugador con la mayor cantidad de rebotes totales.
+    8. Calcular y mostrar el jugador con el mayor porcentaje de tiros de campo.
+    9. Calcular y mostrar el jugador con la mayor cantidad de asistencias totales.
+    10. Permitir al usuario ingresar un valor y mostrar los jugadores que han promediado más puntos por partido que ese valor.
+    11. Permitir al usuario ingresar un valor y mostrar los jugadores que han promediado más rebotes por partido que ese valor.
+    12. Permitir al usuario ingresar un valor y mostrar los jugadores que han promediado más asistencias por partido que ese valor.
+    13. Calcular y mostrar el jugador con la mayor cantidad de robos totales.
+    14. Calcular y mostrar el jugador con la mayor cantidad de bloqueos totales.
+    15. Permitir al usuario ingresar un valor y mostrar los jugadores que hayan tenido un porcentaje de tiros libres superior a ese valor.
+    16. Calcular y mostrar el promedio de puntos por partido del equipo excluyendo al jugador con la menor cantidad de puntos por partido.
+    17. Calcular y mostrar el jugador con la mayor cantidad de logros obtenidos.
+    18. Permitir al usuario ingresar un valor y mostrar los jugadores que hayan tenido un porcentaje de tiros triples superior a ese valor.
+    19. Calcular y mostrar el jugador con la mayor cantidad de temporadas jugadas.
+    20. Permitir al usuario ingresar un valor y mostrar los jugadores, ordenados por posición en la cancha, que hayan tenido un porcentaje de tiros de campo superior a ese valor.
+        """)
+
+
 
         
 def mostrar_jugadores(lista_jugadores:list, mostra_indice : bool) -> list:
@@ -41,10 +67,11 @@ def mostrar_jugadores(lista_jugadores:list, mostra_indice : bool) -> list:
 
 
 def mostrar_estadisticas_jugador_por_indice(lista_jugadores : list):
+
     mostrar_jugadores(lista_jugadores, True)
     indice = input("selecciones el indice del jugador deseado")
     while re.search(r"^(0|1|2|3|4|5|6|7|8|9|10|11)$", indice) == None :
-        print("[error] : numero incorrecto")
+        imprimir_datos("[error] : numero incorrecto")
         indice = input("selecciones el indice del jugador deseado")
 
     indice = int(indice)
@@ -52,8 +79,16 @@ def mostrar_estadisticas_jugador_por_indice(lista_jugadores : list):
     imprimir_datos("el jugador seleccionado es: {0} y sus estadisticas son: {1}".format(
                                                 jugador_seleccionado["nombre"],
                                                 jugador_seleccionado["estadisticas"]))
+    
+    respuesta = input("deseas guardar la informacion en formato csv? (si/no)")
+    if respuesta == "si":
+        guardar_jugadores_en_csv(r"C:\Users\arria\OneDrive\Escritorio\UTN\1er parcial python\PP_lab1_arriagada_kevin\datos.csv", jugador_seleccionado)
 
-
-
+def guardar_jugadores_en_csv(nombre_archivo : str,jugador : list)->None:
+    with open(nombre_archivo, "w") as archivo:
+        texto = "el jugador seleccionado es: {0} y sus estadisticas son: {1}".format(
+                                                jugador["nombre"],
+                                                jugador["estadisticas"])
+        archivo.write(texto)
 
 
---
+++ PP_LAB1_main.py
@@ -0,0 +1,67 @@
+from PP_LAB1 import (
+    imprimir_datos,
+    mostrar_menu,
+    mostrar_estadisticas_jugador_por_indice,
+    lista_jugadores,
+)
+
+
+
+
+
+
+
+
+
+
+def menu_dream_team():
+    while True:
+        mostrar_menu()
+        respuesta = int(input("    seleccionar punto (1 - 20)"))
+        while  respuesta < 1 or respuesta > 20:
+            imprimir_datos("[error] : numero invalido")
+            respuesta = int(input("seleccionar punto (1 - 20)"))
+        match respuesta:
+            case 1:
+                imprimir_datos("vamos bien")
+            case 2:
+                print("hola")
+                mostrar_estadisticas_jugador_por_indice(lista_jugadores)
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                pass
+            case 8:
+                pass
+            case 9:
+                pass
+            case 10:
+                pass
+            case 11:
+                pass
+            case 12:
+                pass
+            case 13:
+                pass
+            case 14:
+                pass
+            case 15:
+                pass
+            case 16:
+                pass
+            case 17:
+                pass
+            case 18:
+                pass
+            case 19:
+                pass
+            case 20:
+                pass
+
+menu_dream_team()
